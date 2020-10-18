5818cb2e433cd6f4621fc5493f33ebf6e9ba3bec
Jacob Louis Lemus Peschel <sirrkitt@gmail.com>
2020-10-17 22:52:35 -0700

    Fix permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ deluser dcc
 addgroup -S -g $GID dcc
 adduser -S -u $UID -h /var/dcc -D -H -s /sbin/nologin --gecos "DCC antispam" -G dcc dcc
 
-chown -R dcc:dcc /socket /data
+chown -R dcc:dcc /socket /data /var/dcc
 
 
 exec /var/dcc/libexec/dccifd -b -t ALL,NEVER,MANY -p /socket/dccifd -h /var/dcc -I dcc -L off -l /data
